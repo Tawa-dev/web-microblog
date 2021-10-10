b305ed666d42aeb1b33dbc9be3b5b908a94bd552
Tawa-dev <81624789+Tawa-dev@users.noreply.github.com>
2021-10-10 02:03:41 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    app.db = client.microblog
+
+
+    @app.route("/",methods=["GET","POST"])
+    def home():
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            fdate = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.article.insert({"content":entry_content,"date":fdate})
+
+        entries_with_dates = [(entry["content"],entry["date"],datetime.datetime.strptime(entry["date"],"%Y-%m-%d").strftime("%b %d")) for entry in app.db.article.find({})]    
+        return render_template("index.html",entries=entries_with_dates) 
+
+    return app
